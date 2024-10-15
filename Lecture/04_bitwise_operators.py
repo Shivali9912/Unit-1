edcb0b40033c337ff2b6d54c22d598d2894f53ee
Shivali9912 <shivalipunjabi@gmail.com>
2024-10-15 06:35:49 -0600

    bitwise operations and loops were added

---
+++ Lecture/04_bitwise_operators.py
@@ -0,0 +1,31 @@
+
+x = 5  # (0101 in binary)
+y = 3  # (0011 in binary)
+# %%
+
+# Bitwise AND 
+print(x & y) #0001
+
+# %%
+# Bitwise OR
+print(x | y) #0111
+
+# %%
+# Bitwise XOR 
+print(x ^ y) # 1001
+
+# %%
+# Bitwise NOT 
+print(~x) #1010
+
+# Bitwise Left Shift #x=0101
+print(x << 1) #x=1010
+
+# Bitwise Right Shift #x=0101
+print(x >> 1) #0010
+
+# Bitwise XNOR
+print(~(x ^ y)) 
+
+
+
---
+++ Lecture/05_loops.py
@@ -0,0 +1,38 @@
+# if-else
+a = 10
+b = 7
+
+if a > b:
+    print('a is greater than b')
+elif a < b:
+    print('a less than b')
+else:
+    print('a and b are equal')
+
+# %%
+# A while loop executes a set of statements as long as a condition is true.
+i = 0
+while i < 10:
+    print(i)
+    i += 1 #i = i+1
+
+i = 0
+a_list = []
+while i < 10:
+    a_list.append(i)
+    i =+ 1
+print(a_list)
+
+
+# %%
+# A for loop is used to iterate over a sequence.
+for i in range(10):
+    print(i)
+
+
+
+# %%
+fruits_list = ['apple', 'orange', 'grapes', 'coconut']
+
+for fruit in fruits_list:
+    print(fruit)
